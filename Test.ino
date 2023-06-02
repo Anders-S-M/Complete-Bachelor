8107c27edb55b506cf3bb1c2866e6c83665d732b
Anders-S-M <92510281+Anders-S-M@users.noreply.github.com>
2023-06-02 10:53:36 +0200

    Update Test.ino

--- Test.ino
+++ Test.ino
@@ -11,80 +11,32 @@ LoRaModem modem;
 // Uncomment if using the Murata chip as a module
 // LoRaModem modem(Serial1);
 
-String appEui;
-String appKey;
-String devAddr;
-String nwkSKey;
-String appSKey;
+String appEui = "XXX"; //Enter your own appEUI and appKey values here
+String appKey = "XXX";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   while (!Serial);
-  Serial.println("Welcome to MKR WAN 1300/1310 first configuration sketch");
-  Serial.println("Register to your favourite LoRa network and we are ready to go!");
-  // change this to your regional band (eg. US915, AS923, ...)
   if (!modem.begin(EU868)) {
     Serial.println("Failed to start module");
     while (1) {}
   };
-  Serial.print("Your module version is: ");
-  Serial.println(modem.version());
-  if (modem.version() != ARDUINO_FW_VERSION) {
-    Serial.println("Please make sure that the latest modem firmware is installed.");
-    Serial.println("To update the firmware upload the 'MKRWANFWUpdate_standalone.ino' sketch.");
-  }
   Serial.print("Your device EUI is: ");
   Serial.println(modem.deviceEUI());
 
-  int mode = 0;
-  while (mode != 1 && mode != 2) {
-    Serial.println("Are you connecting via OTAA (1) or ABP (2)?");
-    while (!Serial.available());
-    mode = 1;
-  }
-
   int connected;
-  if (mode == 1) {
-    Serial.println("Enter your APP EUI");
-    while (!Serial.available());
-    appEui = "0000000000000000";
+  appKey.trim();
+  appEui.trim();
 
-    Serial.println("Enter your APP KEY");
-    while (!Serial.available());
-    appKey = "C8F086720805A5B4E755A3902CFDCEC3";
-
-    appKey.trim();
-    appEui.trim();
-
-    Serial.println("Connecting...");
-    connected = modem.joinOTAA(appEui, appKey);
-  } else if (mode == 2) {
-
-    Serial.println("Enter your Device Address");
-    while (!Serial.available());
-    devAddr = Serial.readStringUntil('\n');
-
-    Serial.println("Enter your NWS KEY");
-    while (!Serial.available());
-    nwkSKey = Serial.readStringUntil('\n');
-
-    Serial.println("Enter your APP SKEY");
-    while (!Serial.available());
-    appSKey = Serial.readStringUntil('\n');
-
-    devAddr.trim();
-    nwkSKey.trim();
-    appSKey.trim();
-
-    connected = modem.joinABP(devAddr, nwkSKey, appSKey);
-  }
+  Serial.println("Connecting...");
+  connected = modem.joinOTAA(appEui, appKey);
 
   if (!connected) {
     Serial.println("Something went wrong; are you indoor? Move near a window and retry");
     while (1) {
       Serial.println("Connecting...");
-      connected = modem.joinABP(devAddr, nwkSKey, appSKey);}
+      connected = modem.joinOTAA(appEui, appKey);}
   }
 
   delay(5000);
